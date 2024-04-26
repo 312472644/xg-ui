fda39f94872a5c9d497a1c559a46c98a84e6a44a
xiong xiong <xx312472644@163.com>
2024-04-26 16:00:32 +0800

    docs:icon图片修改

--- docs/.vitepress/base-config.mjs
+++ docs/.vitepress/base-config.mjs
@@ -5,5 +5,5 @@ export default {
   description: 'Xg UI使用文档说明',
   lastUpdated: false,
   base: '/xg-ui/',
-  head: [['link', { rel: 'icon', href: '/orange.png' }]],
+  head: [['link', { rel: 'icon', href: '/xg-ui/orange.png' }]],
 };
